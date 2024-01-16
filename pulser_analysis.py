7a892a7a23654f331889fbadc9caeb6a45b6b7a5
fraborra <borra.francesco@gmail.com>
2024-01-16 16:36:44 +0100

    first changes

--- pulser_analysis.py
+++ pulser_analysis.py
@@ -35,27 +35,29 @@ def main():
     arg, st, sf = par.add_argument, "store_true", "store_false"
     arg("-dc", "--daq_conversion", action=st, help="daq_conversion")
     arg("-r", "--run", nargs=1, action="store", help = "data run")
+    arg("-p", "--period", nargs=1, action="store", help = "data period")
     arg("-na", "--acq_number", nargs=1, help="number of acquisition")
     args = vars(par.parse_args())
     
     local_dir = os.path.dirname(os.path.realpath(__file__))
     
     if args["run"]:
-        period, run = int(args["run"][0]), int(args["run"][1])
+        period = int(args["run"][0])
+        run    = int(args["period"][0])
         run, period = f'r{run:03}', f'p{period:02}'
     else:
         print('Run and period not provided')
         return
     run_dir = f'{local_dir}/l200-{period}/{run}'
     
-    log_dir = local_dir + '/_out'
+    log_dir = local_dir + '/log_out'
     try: os.mkdir(log_dir)
     except: FileExistsError: print ("Directory '%s' already exists" % log_dir)
     else: print ("Directory '%s' created" % log_dir)
     
     
-    n_test = int(args["test_number"][0])
-    n_acq = int(args["acq_number"][0])
+#    n_test = int(args["test_number"][0])
+#    n_acq = int(args["acq_number"][0])
     daq_conv = False
     if args['daq_conversion']: daq_conv = True
         
@@ -69,43 +71,81 @@ def main():
     except: FileExistsError: print (f'Directory {dsp_dir} already exists')
     else: print(f'Directory {dsp_dir} created')
 
-    raw_dir = f'/lfs/l1/legend/users/dandrea/test_data/raw/tst/{period}-{run}'
+#    raw_dir = f'/lfs/l1/legend/users/dandrea/test_data/raw/tst/{period}-{run}'
+    raw_dir = '/Volumes/Pennetta_ex/p06-r007'
+
     
     # metadata
     version = 'tmp/auto'
-    meta_path = f'/lfs/l1/legend/data/public/prodenv/prod-blind/{version}/inputs/'
+#    meta_path = f'/lfs/l1/legend/data/public/prodenv/prod-blind/{version}/inputs/'         # REAL PATH
+#    meta_path = '~/temp_files/legend-metadata'                                              # TEMP path
+    meta_path = f'/Users/francescoborra/software/legend_200/legend-metadata'
+
+    
     lmeta = LegendMetadata(path=meta_path)
     chmap = lmeta.hardware.configuration.channelmaps.on("20230311T235840Z")
+
     dsp_proc_chain = lmeta.dataprod.config.tier_dsp['L200-p01-r%-T%-ICPC-dsp_proc_chain']
-    #par_dsp = lmeta.dataprod.overrides.dsp.cal.p03.r000['L200-p03-r000-cal-T%-par_dsp-overwrite']
-    par_dir = f'/lfs/l1/legend/data/public/prodenv/prod-blind/{version}/generated/par'
-    par_dsp = f'{par_dir}/dsp/cal/p06/r005/l200-p06-r005-cal-20230717T130655Z-par_dsp.json'
-    par_hit = f'{par_dir}/hit/cal/p06/r005/l200-p06-r005-cal-20230717T130655Z-par_hit.json'
+
+#    par_dir = f'/lfs/l1/legend/data/public/prodenv/prod-blind/{version}/generated/par'
+#    par_dsp = f'{par_dir}/dsp/cal/p06/r005/l200-p06-r005-cal-20230717T130655Z-par_dsp.json'           # REAL par_dsp
+    par_dsp = lmeta.dataprod.overrides.dsp.cal.p03.r000['L200-p03-r000-cal-T%-par_dsp-overwrite']      # TEMP par_dsp
+
+#    par_hit = f'{par_dir}/hit/cal/p06/r005/l200-p06-r005-cal-20230717T130655Z-par_hit.json'
+
+
+
+    chmap = lmeta.hardware.configuration.channelmaps.on("20230311T235840Z")
+
+    ge_all = [ch for ch in chmap.keys() if chmap[ch]['system']=='geds']
+    ge_off = ['V07298B', 'P00665A', 'V01386A', 'V01403A', 'V01404A', 'B00091D', 'P00537A',
+              'B00091B', 'P00538B', 'P00661A', 'P00665B', 'P00698B']
+    ge_keys = [ch for ch in ge_all if ch not in ge_off]
+    ge_rawid = [chmap[ch]['daq']['rawid']  for ch in ge_keys]
+    ge_table = [f'ch{rawid}' for rawid in ge_rawid]
     
     test_dict_file = f'l200-{period}/{run}/l200-{period}-{run}-pulser-dict.json'
     with open(test_dict_file,'r') as file:
         test_dict = json.load(file)
-    this_dict = test_dict[f'test{n_test}'][f'{n_acq}']
-    print('Analysis of pulser test',n_test,'acquistion',n_acq)
-    lh5_file = f'{dsp_dir}/l200-{period}-{run}_pulser-test_acq{n_acq}.lh5'
+#    this_dict = test_dict[f'test{n_test}'][f'{n_acq}']
+#    print('Analysis of pulser test',n_test,'acquistion',n_acq)
+#    lh5_file = f'{dsp_dir}/l200-{period}-{run}_pulser-test_acq{n_acq}.lh5'
     
-    this_dict = test_dict[f'test{n_test}'][f'{n_acq}']
-    key_list = [f'l200-p02-r{run:03}-tst-{key}' for key in this_dict['key_list']]
-    raw_list = [f'{raw_dir}/{key}_raw.lh5' for key in key_list]
-    print('raw_list:',raw_list)
-    pulser_waveform(raw_list, chmap, dsp_config, hit_config, test_dict_file, n_test=n_test, n_acq=n_acq,
-                    wcut=2100, plot_dir=plot_dir,lh5_file=lh5_file)
+#    this_dict = test_dict[f'test{n_test}'][f'{n_acq}']
+#    key_list = [f'l200-p02-r{run:03}-tst-{key}' for key in this_dict['key_list']]
+#    raw_list = [f'{raw_dir}/{key}_raw.lh5' for key in key_list]
+
+    key_list = open(f'l200-{period}/{run}/l200-{period}-{run}-tst-key.list', "r")                     # FRANCESCO
+    key_list = key_list.read().split('\n')
+    raw_list = [f'{raw_dir}/l200-{period}-{run}-tst-{key}-tier_raw.lh5' for key in key_list]
+
+#    print('raw_list:',raw_list)
+#    pulser_waveform(raw_list, chmap, dsp_config, hit_config, test_dict_file, n_test=n_test, n_acq=n_acq,
+#                    wcut=2100, plot_dir=plot_dir,lh5_file=lh5_file)
+
+    for ge_key in ge_keys:                                                                            # FRANCESCO
+            pulser_processing(period, run, ge_key, test_dict_file, raw_dir, plot_dir = plot_dir)
+            print('Finito un giro', flush=True)
+            print('\n\n ===================================== \n\n \n\n', flush = True)
 
 
 def plot_pulser_waveforms(ge_key, det_name, raw_list, puls_chn = 'ch1027201',
                           nwf=10, us=1000/16, xlim=(3060,3090), ylim=(1e4,1e9), plot_dir=None ):
-    meta_path = f'/lfs/l1/legend/users/dandrea/legend-metadata'
+
+    meta_path = f'/Users/francescoborra/software/legend_200/legend-metadata'
     lmeta = LegendMetadata(path=meta_path)
     chmap = lmeta.hardware.configuration.channelmaps.on("20230311T235840Z")
+
+#     chmap_file = './l200-p03-r%-T%-all-config.json'
+#     with open(chmap_file,'r') as file:
+#         chmap = json.load(file)
+    
     rawid = chmap[ge_key]['daq']['rawid']
     chn = f'ch{rawid}'
     store = lh5.LH5Store()
     daq_puls, n_rows = store.read_object(f"{puls_chn}/raw/daqenergy", raw_list)
+#     daq_puls, n_rows = store.read_object(f"./l200-p06-r007-tst-20230726T103316Z-tier_raw.lh5", raw_list)
+    
     daq_puls = daq_puls.nda
     pulser_tag = daq_puls>1000
     waveforms, n_rows = store.read_object(f"{chn}/raw/waveform/values", raw_list, idx=pulser_tag)
@@ -129,33 +169,62 @@ def plot_pulser_waveforms(ge_key, det_name, raw_list, puls_chn = 'ch1027201',
 
 
 def pulser_processing(period, run, ge_key, test_dict_file, raw_dir, puls_chn = 'ch1027201',
-                      nwf = 10, us = 1000/16, plot_dir = None ):
+                      nwf = 10, us = 1000/16, plot = False, plot_dir = None, show_plot = False):
     with open(test_dict_file,'r') as file:
         test_dict = json.load(file)
-    meta_path = f'/lfs/l1/legend/users/dandrea/legend-metadata'
+        
+    meta_path = f'/Users/francescoborra/software/legend_200/legend-metadata'
     lmeta = LegendMetadata(path=meta_path)
+        
     chmap = lmeta.hardware.configuration.channelmaps.on("20230311T235840Z")
+#     chmap_file = './l200-p03-r%-T%-all-config.json'
+#     with open(chmap_file,'r') as file:
+#         chmap = json.load(file)
+
     dsp_proc_chain = lmeta.dataprod.config.tier_dsp['L200-p01-r%-T%-ICPC-dsp_proc_chain']
+#     dsp_proc_chain_file = './l200-p01-r%-T%-ICPC-dsp_proc_chain.json'
+#     with open(dsp_proc_chain_file,'r') as file:
+#         dsp_proc_chain = json.load(file)
+
     par_dsp = lmeta.dataprod.overrides.dsp.cal.p03.r000['L200-p03-r000-cal-T%-par_dsp-overwrite']
+#     par_dsp_file = './l200-p03-r000-cal-T%-par_dsp-overwrite.json'
+#     with open(par_dsp_file,'r') as file:
+#         par_dsp = json.load(file)
+
+    local_dir = os.path.dirname(os.path.realpath(__file__))
+    run_dir = f'{local_dir}/l200-{period}/{run}'
+    dsp_dir = run_dir + '/dsp'
+    try: os.mkdir(dsp_dir)
+    except: FileExistsError: print (f'Directory {dsp_dir} already exists', flush=True)
+    else: print(f'Directory {dsp_dir} created', flush=True)
+
     rawid = chmap[ge_key]['daq']['rawid']
     chn = f'ch{rawid}'
     store = lh5.LH5Store()
     
-    fig, axis = plt.subplots(nrows=2,ncols=4,figsize=(20,8), facecolor='white')
-    fig1, axis1 = plt.subplots(nrows=2,ncols=4,figsize=(20,8), facecolor='white')
+    if plot:
+        fig, axis = plt.subplots(nrows=2,ncols=4,figsize=(20,8), facecolor='white')
+        fig1, axis1 = plt.subplots(nrows=2,ncols=4,figsize=(20,8), facecolor='white')
+    
     string = int(chmap[ge_key]['location']['string'])
     cc4_id = chmap[ge_key]['electronics']['cc4']['id']
     attenuations = np.array(test_dict[cc4_id]['attenuations'])
     out_voltages = np.array([pulser_db_in_voltage(att) for att in attenuations])
-    print(out_voltages)
+#    print(out_voltages)
     rough_energies = np.array([pulser_db_in_voltage(att,energy=1) for att in attenuations])
     amplitudes = np.array(test_dict[cc4_id]['amplitudes'])
     keys = test_dict[cc4_id]['keys']
+    
+    if len(keys)!= len(rough_energies):
+        print('There is a problem with the lenght of the attenuation array', flush=True)
+        
     pulser_pos = np.zeros(len(keys))
-    print('Pulser processing for',ge_key,'String',string,'CC4',cc4_id)
+    print('Pulser processing for',ge_key,'String',string,'CC4',cc4_id, flush=True)
+        
     for i, key in enumerate(keys[:]):
         raw_file = f'{raw_dir}/l200-{period}-{run}-tst-{key}-tier_raw.lh5'
         daq_puls, n_rows = store.read_object(f"{puls_chn}/raw/daqenergy", raw_file)
+#        daq_puls = daq_puls
         daq_puls = daq_puls.nda
         pulser_tag = daq_puls>1000
         
@@ -163,19 +232,20 @@ def pulser_processing(period, run, ge_key, test_dict_file, raw_dir, puls_chn = '
         timestamp, n_rows = store.read_object(f"{chn}/raw/timestamp", raw_file, idx=pulser_tag)
         baseline, n_rows = store.read_object(f"{chn}/raw/baseline", raw_file, idx=pulser_tag)
         
-        n_ev, wsize = waveforms.values.nda.shape
-        dts = np.linspace(0,wsize/us,wsize)
-        print(key, 'n. pulser events',n_ev,'size',wsize)
-        ax = axis.flat[i]
-        axin = ax.inset_axes([0.55, 0.1, 0.4, 0.55])
-        for j in range(nwf):
-            wf = waveforms.values.nda[j]
-            ax.plot(dts,wf,label=f'{j} {(timestamp.nda[j]-timestamp.nda[0])*1e3:.2f} ms')
-            axin.plot(dts,wf)
-        ax.set_xlabel('time ($\mu$s)')
-        ax.legend(title=f'{ge_key} - CC4-{cc4_id}\nPulser att. {attenuations[i]} dB',loc='upper left')
-        axin.set_xlim(40.4,55.2)
-        axin.set_yticklabels('')
+        if plot:
+            n_ev, wsize = waveforms.values.nda.shape
+            dts = np.linspace(0,wsize/us,wsize)
+            print(key, 'n. pulser events',n_ev,'size',wsize)
+            ax = axis.flat[i]
+            axin = ax.inset_axes([0.55, 0.1, 0.4, 0.55])
+            for j in range(nwf):
+                wf = waveforms.values.nda[j]
+                ax.plot(dts,wf,label=f'{j} {(timestamp.nda[j]-timestamp.nda[0])*1e3:.2f} ms')
+                axin.plot(dts,wf)
+            ax.set_xlabel('time ($\mu$s)')
+            ax.legend(title=f'{ge_key} - CC4-{cc4_id}\nPulser att. {attenuations[i]} dB',loc='upper left')
+            axin.set_xlim(40.4,55.2)
+            axin.set_yticklabels('')
         
         # DSP production
         start_t = time.time()
@@ -188,22 +258,28 @@ def pulser_processing(period, run, ge_key, test_dict_file, raw_dir, puls_chn = '
         print(f'Time to produce DSP data {time.time()-start_t:.1f} s')
         
         # energy spectrum
-        energies = dsp_data['cuspEmax'].nda
+        energies = dsp_data['cuspEmax']
         ene_mean = np.mean(energies)
-        ene_space = np.linspace(ene_mean-100,ene_mean+100,500)
+        if plot: ene_space = np.linspace(ene_mean-100,ene_mean+100,500)
         pulser_pos[i] = ene_mean
-        ax1 = axis1.flat[i]
-        ax1.hist(energies,bins=ene_space,label='data')
-        ax1.set_xlabel('uncalibrated energy (ADC)')
-        ax1.legend(title=f'{ge_key} - CC4-{cc4_id}\nPulser att. {attenuations[i]}',loc='upper left')
         
-        #hit_config = hit_config_prod
-        #print('Using hit config',hit_config)
-        #with open(hit_config, "r") as file:
-        #    hit = json.load(file)
-        #m_cal = hit[chn_old]['operations']['cuspEmax_ctc_cal']['parameters']['a']
-        #q_cal = hit[chn_old]['operations']['cuspEmax_ctc_cal']['parameters']['b']
-        #cusp_ene = m_cal * cusp_ene + q_cal
+        if plot:
+            ax1 = axis1.flat[i]
+            ax1.hist(energies,bins=ene_space,label='data')
+            ax1.set_xlabel('uncalibrated energy (ADC)')
+            ax1.legend(title=f'{ge_key} - CC4-{cc4_id}\nPulser att. {attenuations[i]}',loc='upper left')
+
+        # Save the "normal_dsp" for the analysis
+        dsp_out_file = f'./l200-p06/{run}/dsp/big/l200-{period}-{run}-pulser_test-{key}-dsp.lh5'
+        store.write_object(dsp_data, name="dsp", lh5_file=dsp_out_file, wo_mode='overwrite')
+
+#        hit_config = hit_config_prod
+#        print('Using hit config',hit_config)
+#        with open(hit_config, "r") as file:
+#            hit = json.load(file)
+#        m_cal = hit[chn_old]['operations']['cuspEmax_ctc_cal']['parameters']['a']
+#        q_cal = hit[chn_old]['operations']['cuspEmax_ctc_cal']['parameters']['b']
+#        cusp_ene = m_cal * cusp_ene + q_cal
         
         """
         test_dict[f'test{n_test}'][f'{n_acq}'][chn] = {'position':pos[i],
@@ -225,6 +301,9 @@ def pulser_processing(period, run, ge_key, test_dict_file, raw_dir, puls_chn = '
         ax3.plot(freq[1:], power[1:],label=det)
         
         print()
+        
+        lh5_file = f'{dsp_dir}/l200-{period}-{run}_pulser-test_acq1.lh5'
+
         if lh5_file is not None:
             store = lh5.LH5Store()
             try:
@@ -232,17 +311,38 @@ def pulser_processing(period, run, ge_key, test_dict_file, raw_dir, puls_chn = '
                 print('lh5 file already existing')
             except:
                 print('new lh5 file')
-            store.write_object(Array(cusp_ene), name="pulser_energy", lh5_file=lh5_file,
+#            store.write_object(Array(cusp_ene), name="pulser_energy", lh5_file=raw_file,
+#                               wo_mode='overwrite',group=chn)
+            store.write_object(Array(energies), name="pulser_energy", lh5_file=lh5_file,
                                wo_mode='overwrite',group=chn)
             store.write_object(Array(timestamp), name="timestamp", lh5_file=lh5_file,
                                wo_mode='overwrite',group=chn)
         """
-    fig2, ax2 = plt.subplots(figsize=(12,6.75), facecolor='white')
-    ax2.plot(out_voltages,pulser_pos,label='data')
-    ax2.legend(title=f'{ge_key} - CC4-{cc4_id}')
-    ax2.set_xlabel('pulser voltages (V)')
-    ax2.set_ylabel('pulser position (ADC)')
+    
+    
+    # Save the "small_dsp" for the analysis
+    out_lh5_cc4 = f'./l200-p06/{run}/dsp/small/l200-{period}-{run}-small_dsp-{cc4_id}.lh5'
 
+    store.write_object(Array(rough_energies), name="puls ene", lh5_file=out_lh5_cc4,
+                       group=f'{chn}', wo_mode='overwrite')
+    store.write_object(Array(amplitudes), name="amplitudes", lh5_file=out_lh5_cc4,
+                       group=f'{chn}', wo_mode='overwrite')
+    store.write_object(Array(attenuations), name="attenuations", lh5_file=out_lh5_cc4,
+                       group=f'{chn}', wo_mode='overwrite')
+    store.write_object(Array(pulser_pos), name="uncal ene", lh5_file=out_lh5_cc4,
+                       group=f'{chn}', wo_mode='overwrite')
+
+    
+    if plot:
+        fig2, ax2 = plt.subplots(figsize=(12,6.75), facecolor='white')
+        ax2.plot(out_voltages,pulser_pos,label='data')
+        ax2.legend(title=f'{ge_key} - CC4-{cc4_id}')
+        ax2.set_xlabel('pulser voltages (V)')
+        ax2.set_ylabel('pulser position (ADC)')
+    
+    if plot and show_plot: plt.show()
+    
+    
 def pulser_db_in_voltage(attenuation_db, pulser_voltage = 2.5, energy = False):
     ratio = 10.0**(-attenuation_db / 20.0)
     output = ratio * pulser_voltage
@@ -326,14 +426,17 @@ def process_calibration(chn, file_dir, sigma, flat, tau, n_events = 50000, wcut=
                 
 def pulser_dsp_analysis(run, chmap, test_dict_file, n_test = 1, n_acq = 0, nstring = None, plot_dir = None ):
     store = lh5.LH5Store()
-    lh5_file = f'r{run:03}/pulser_test/dsp/l200_r{run:03}_pulser_test{n_test}_acq{n_acq}.lh5'
+    lh5_file = f'./l200-p06/{run}/dsp/l200_{run}_pulser_test_acq.lh5'
+#    lh5_file = f'{raw_dir}/l200-p{06}-{run}-tst-{key}-tier_raw.lh5'
+#    lh5_file = f'r{run:03}/pulser_test/dsp/l200_r{run:03}_pulser_test{n_test}_acq{n_acq}.lh5'
     with open(test_dict_file,'r') as file:
         test_dict = json.load(file)
     fig0, axis0 = plt.subplots(figsize=(20,8), facecolor='white')
     fig1, axis1 = plt.subplots(figsize=(20,8), facecolor='white')
     fig2, axis2 = plt.subplots(nrows=4, ncols=3,figsize=(24,16), facecolor='white')
     chn_off = (26,33,46,47,48,50,84,85,94,112,116,117,120,122)
-    ge_off = ['V07298B', 'P00665A', 'V01386A', 'V01403A', 'V01404A', 'B00091D', 'P00537A', 'B00091B', 'P00538B', 'P00661A', 'P00665B', 'P00698B']
+    ge_off = ['V07298B', 'P00665A', 'V01386A', 'V01403A', 'V01404A', 'B00091D', 'P00537A', 
+              'B00091B', 'P00538B', 'P00661A', 'P00665B', 'P00698B']
     ge_drift = ['V01406A', 'V01415A', 'V01387A', 'P00665C','P00748B', 'P00748A']
     strings = np.array([int(chmap[ch]['location']['string']) for ch in chmap.keys() if chmap[ch]['system']=='geds'])
     if nstring is None: nstring = strings.max()
@@ -341,16 +444,14 @@ def pulser_dsp_analysis(run, chmap, test_dict_file, n_test = 1, n_acq = 0, nstri
         print('string',string)
         ax2 = axis2.flat[string-1]
         ge_keys = [ch for ch in chmap.keys() if chmap[ch]['system']=='geds' and chmap[ch]['location']['string']==string]
-        if run < 20:
-            ge_keys = [ch for n, ch in zip(ge_numbers,det_names) if n not in chn_off ]
-        else:
-            ge_keys = [ch for ch in ge_keys if ch not in ge_off]
-            ge_keys = [ch for ch in ge_keys if ch not in ge_drift]
-        ge_numbers = [chmap[ch]['daq']['fcid'] for ch in ge_keys]
-        chns = [f'ch{n:03}' for n in ge_numbers]
+        ge_keys = [ch for ch in ge_keys if ch not in ge_off]
+        ge_keys = [ch for ch in ge_keys if ch not in ge_drift]
+#        ge_numbers = [chmap[ch]['daq']['fcid'] for ch in ge_keys]
+#        chns = [f'ch{n:03}' for n in ge_numbers]
         ge_rawid = [chmap[ch]['daq']['rawid']  for ch in ge_keys]
-        if run < 20: ge_table = chns
-        else: ge_table = [f'ch{id}' for id in ge_rawid]
+        
+        ge_table = [f'ch{id}' for id in ge_rawid]
+        
         fwhm, fwhm_err = np.zeros(len(ge_table)), np.zeros(len(ge_table))
         pos, pos_err = np.zeros(len(ge_table)), np.zeros(len(ge_table))
         #fig, axis = plt.subplots(nrows=4, ncols=3,figsize=(24,16), facecolor='white')
